c10fcf945e30a4c19a89c66a1b93bb5ee511446e
Fallen_Breath <yqx791125833@gmail.com>
2023-10-29 19:53:05 +0800

    v0.1.1

--- src/main/java/me/fallenbreath/velocityrememberserver/PluginMeta.java
+++ src/main/java/me/fallenbreath/velocityrememberserver/PluginMeta.java
@@ -4,5 +4,5 @@ public class PluginMeta
 {
 	public static final String ID = "velocityrememberserver";
 	public static final String NAME = "VelocityRememberServer";
-	public static final String VERSION = "0.1.0";
+	public static final String VERSION = "0.1.1";
 }
